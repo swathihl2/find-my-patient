05ba9c39d33835150adf1774d5942f585e8d16df
Swathi HL <swathihl916@gmail.com>
2022-06-07 20:28:22 +0530

    Updates

---
+++ precache-manifest.0193cda8917b431af34cee2408187e39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61584709ea85f32977d2761045616c3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37d054dab1593faa1cf4",
+    "url": "/static/css/2.f37fde72.chunk.css"
+  },
+  {
+    "revision": "a96a3875f224c6d37ae7",
+    "url": "/static/css/main.9e070e31.chunk.css"
+  },
+  {
+    "revision": "37d054dab1593faa1cf4",
+    "url": "/static/js/2.a4f93fcd.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.a4f93fcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a96a3875f224c6d37ae7",
+    "url": "/static/js/main.57d8d0c3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.57d8d0c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
